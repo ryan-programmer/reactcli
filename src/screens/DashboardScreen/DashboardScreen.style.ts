becd360ade726d00affd4d711308ca44ac114a7c
Ryan <59949601+ryan-programmer@users.noreply.github.com>
2025-09-08 15:29:03 +0500

    dashboard changes

--- src/screens/DashboardScreen/DashboardScreen.style.ts
+++ src/screens/DashboardScreen/DashboardScreen.style.ts
@@ -1,88 +1,112 @@
 import { StyleSheet } from "react-native";
-
+import { Dimensions } from "react-native";
 export default StyleSheet.create({
-container:{
-    flex:1,
-    borderWidth:1,
-    borderColor:"black"
-},
-topBar:{
-    flexDirection:"row",
-    justifyContent:"space-between",
-    alignItems:"center",
-    // borderWidth:1,
-    // borderColor:"red",
-    padding:10
-},
-currAccount:{
-    flexDirection:"column",
-    justifyContent:"center",
-    alignItems:"center",
-    borderWidth:1,
-    borderColor:"blue",
-    alignSelf:"flex-start",
-    marginStart:10,
-    padding:10
-},
-boxOptionsView:{
-    flexDirection:"row",
-    borderWidth:1,
-    borderColor:"green",
-    justifyContent:"space-evenly",
-    alignItems:"center",
-    padding:5
-},
-boxOptions:{
-    flexDirection:"column",
-    justifyContent:"center",
-    alignItems:"center",
-    borderWidth:1,
-    borderColor:"blue",
-    alignSelf:"flex-start",
-    padding:10
-},
-quickActionsView:{
-    flexDirection:"column",
-    justifyContent:"center",
-    alignItems:"flex-start",
-    borderWidth:1,
-    borderColor:"black",
-},
+    container: {
+        flex: 1,
+        borderWidth: 1,
+        borderColor: "black",
+        backgroundColor:"white"
+    },
+    topSection: {
+        height: Dimensions.get("screen").height * 0.25,
+        width: Dimensions.get("screen").width * 0.99,
+        paddingHorizontal:1,
+        // borderWidth: 1,
+        // borderColor: "blue",
+        backgroundColor:"#f8f4f4ff"
+    },
+    topBar: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        // borderWidth:1,
+        // borderColor:"red",
+        padding: 10
+    },
+    currAccount: {
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "flex-start",
+        borderWidth: 1,
+        borderColor: "blue",
+        alignSelf: "flex-start",
+        marginStart: 10,
+        padding: 10
+    },
+    boxOptionsView: {
+        flexDirection: "row",
+        width:"100%",
+        borderWidth: 1,
+        borderColor: "green",
+        justifyContent: "space-evenly",
+        alignItems: "center",
+        padding: 5,
+        position: "absolute",
+        marginTop: Dimensions.get("screen").height * 0.20
 
-quickActionsRow:{
-    flexDirection:"row",
-    justifyContent:"center",
-    alignItems:"flex-start",
-    // borderWidth:1,
-    // borderColor:"black",
-    paddingStart:5
-},
-quickActions:{
-    flexDirection:"column",
-    justifyContent:"center",
-    alignItems:"center",
-    // borderWidth:1,
-    // borderColor:"black",
-    padding:22,
-    marginStart:10,
-},
-quickActionsCircle:{
-    padding:20,
-    borderWidth:1,
-    borderColor:"red",
-    borderRadius:30
-},
-profileCircle:{
-    backgroundColor:"#951c07",
-    color:"white",
-    padding:8,
-    borderRadius:18
-},
-tapbarRight:{
-    flexDirection:"row",
-    justifyContent:"space-between",
-    // borderWidth:1,
-    // borderColor:"red",
-    gap:10    
-}
+    },
+    boxOptions: {
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        borderWidth: 1,
+        borderColor: "blue",
+        // alignSelf: "center",
+        padding: 17,
+        marginLeft:10,
+    },
+    quickActionsView: {
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "flex-start",
+        borderWidth: 1,
+        borderColor: "black",
+        marginTop: Dimensions.get("screen").height * 0.07
+
+    },
+
+    quickActionsRow: {
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        borderWidth:1,
+        borderColor:"black",
+        paddingStart: 5
+    },
+    quickActions: {
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        // borderWidth:1,
+        // borderColor:"black",
+        padding: 22,
+        // marginStart: 10,
+    },
+    quickActionsCircle: {
+        padding: 20,
+        borderWidth: 1,
+        borderColor: "red",
+        borderRadius: 30
+    },
+    profileCircle: {
+        backgroundColor: "#951c07",
+        color: "white",
+        padding: 8,
+        borderRadius: 18
+    },
+    tapbarRight: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        // borderWidth:1,
+        // borderColor:"red",
+        gap: 10
+    },
+    promoBox:{
+        borderWidth:1,
+        borderColor:"red",
+        flexDirection:"row",
+        justifyContent:"space-between",
+        padding:8
+
+    }
 })
--- src/screens/DashboardScreen/DashboardScreen.tsx
+++ src/screens/DashboardScreen/DashboardScreen.tsx
@@ -1,50 +1,59 @@
 import React from 'react'
-import { Image, Text, View } from 'react-native'
+import { Button, Image, Text, View } from 'react-native'
 import styles from './DashboardScreen.style'
 import { logo, searchIcon } from '../../../assets/images'
+import { Dimensions } from 'react-native'
 const DashboardScreen = () => {
+  console.log("Dim" + Dimensions.get("screen").height * 10);
+
   return (
     <View style={styles.container} >
-      <View style={styles.topBar}>
-        <View style={styles.profileCircle}> <Text style={{ color: "white" }}>CU</Text></View>
 
-        <View style={styles.tapbarRight}>
-          <Image source={searchIcon} style={{ height: 25, width: 25 }} />
-          <Image source={logo} style={{ height: 25, width: 25 }} />
+      <View style={styles.topSection}>
+        <View style={styles.topBar}>
+          <View style={styles.profileCircle}> <Text style={{ color: "white" }}>CU</Text></View>
+
+          <View style={styles.tapbarRight}>
+            <Image source={searchIcon} style={{ height: 25, width: 25 }} />
+            <Image source={logo} style={{ height: 25, width: 25 }} />
+          </View>
+
         </View>
 
+
+        <View style={styles.currAccount}>
+          <Text>Current  Account | *0050</Text>
+          <Text>* * * * PKR </Text>
+          <Button title='Overview' color={"red"}></Button>
+        </View>
       </View>
-
-
-      <View style={styles.currAccount}>
-        <Text>one</Text>
-        <Text>two</Text>
-        <Text>three</Text>
-      </View>
-
 
       <View style={styles.boxOptionsView}>
         <View style={styles.boxOptions}>
           <Text>one</Text>
-          <Text>two</Text>
-          <Text>three</Text>
+          <Text style={{ flexWrap: "wrap" }}>Send Money</Text>
         </View>
 
 
         <View style={styles.boxOptions}>
           <Text>one</Text>
           <Text>two</Text>
-          <Text>three</Text>
+
         </View>
 
 
         <View style={styles.boxOptions}>
           <Text>one</Text>
           <Text>two</Text>
-          <Text>three</Text>
+        </View>
+
+        <View style={styles.boxOptions}>
+          <Text>one</Text>
+          <Text>two</Text>
         </View>
 
       </View>
+
       <View style={styles.quickActionsView}>
 
 
@@ -80,6 +89,19 @@ const DashboardScreen = () => {
 
         </View>
 
+
+      </View>
+      <View style={styles.promoBox}>
+        <View style={{ flexDirection: "row" }}>
+          <Image source={logo} style={{ height: 25, width: 25 }} />
+          <View style={{padding:4,marginLeft:4}}>
+            <Text style={{fontWeight:"600",fontSize:16}}>Instant Loan upt to 750,000</Text>
+            <Text>asd</Text>
+
+
+          </View>
+        </View>
+        <Image source={searchIcon} style={{ height: 25, width: 25 }} />
 
       </View>
 
